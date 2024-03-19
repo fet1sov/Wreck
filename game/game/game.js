2bc42b121e611becf597ca2079837696754ed7ec
fet1sov <august9222@gmail.com>
2024-03-19 22:09:28 +0500

    World: Test render of bush in the world
    Added the world rendering and other features

--- game/game/game.js
+++ game/game/game.js
@@ -1,16 +1,43 @@
+import { World } from "./world/world.js";
+
+/**
+*   Game
+*   Main game class
+*
+*   @author fet1sov <prodaugust21@gmail.com>
+*/
+
 export class Game {
-    constructor(canvas, ctx) {
+    /**
+     * Game instance constructor
+     * It creating the world and initialize basic resource
+     * @param {any} Canvas document element
+     * @param {CanvasRenderingContext2D} 2D context of canvas
+     * @param {Resources} Game assets class
+     */  
+    constructor(canvas, ctx, resources) {
         this.canvas = canvas;
         this.ctx = ctx;
+        this.resources = resources;
+
+        this.world = new World("#60BC23", 32);
     }
 
+    /**
+     * Resize game window method
+     * This method resize canvas window in body of DOM Tree
+     * @param {number} Width of the window
+     * @param {number} Height of the window
+     */  
     resizeWindow(width, height) {
         this.canvas.width = width;
         this.canvas.height = height;
     }
 
+    /**
+     * Game rendering method
+     */  
     render() {
-        this.ctx.fillStyle = "#60BC23";
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.world.render(this.ctx, this.resources);
     }
 }
--- game/game/index.js
+++ game/game/index.js
@@ -1,20 +1,7 @@
 import { Game } from "./game.js";
+import { resources } from "./resources.js";
 
 window.onload = function () {
-    const heads = document.getElementsByTagName("head");
-    if (heads && heads.length > 0) {
-        const requireScripts = [
-            "game/game.js",
-        ];
-
-        for (let i = 0; i < requireScripts.length; i++) {
-            var script = document.createElement("script");
-            script.setAttribute("type", "module");
-            script.setAttribute("src", requireScripts[i]);
-            heads[0].appendChild(script);
-        }
-    }
-
     const canvas = document.createElement("canvas");
     canvas.id = "gameWindow";
     canvas.width = window.innerWidth;
@@ -22,7 +9,7 @@ window.onload = function () {
 
     const ctx = canvas.getContext("2d");
 
-    let game = new Game(canvas, ctx);
+    let game = new Game(canvas, ctx, resources);
     document.body.append(canvas);
 
     window.addEventListener('resize', resizeCanvas, false);
@@ -37,22 +24,4 @@ window.onload = function () {
     }
       
     draw();
-}
-
-function setCookie(name, value, exp) {
-    var date = new Date();
-    date.setTime(date.getTime() + exp * 24 * 60 * 60 * 1000);
-    document.cookie =
-        name + "=" + value + ";expires=" + date.toUTCString() + ";path=/";
-}
-
-function getCookie(name) {
-    var value = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
-    return value ? value[2] : null;
-}
-
-function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        navigator.userAgent
-    );
 }
